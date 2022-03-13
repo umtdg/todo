62df95d37a4bc9e155e0f0762c382f61f9a02539
Umut Dag <dagumut604@gmail.com>
2022-03-13 17:56:05 +0300

    Move some functions from main to utils

--- src/main.rs
+++ src/main.rs
@@ -4,73 +4,19 @@ mod tags;
 mod tasklist;
 
 use std::borrow::Borrow;
-use clap::{Arg, ArgMatches, Command};
-use std::env::var;
-use std::path::Path;
+use clap::{Arg, Command};
 use std::process::exit;
-use colored::Colorize;
-use crate::tags::Tags;
-use crate::task::{PRIORITY_DEFAULT, PRIORITY_MAX, PRIORITY_MIN, Task};
+use itertools::Itertools;
+use crate::task::Task;
 use crate::tasklist::TaskList;
-use crate::utils::NumberOfDigits;
-
-const ERR_SUCCESS: i32 = 0;
-const ERR_SAVE: i32 = 1;
-const ERR_INVALID_TASK_ID: i32 = 2;
-const ERR_COMPLETE: i32 = 3;
-const ERR_START: i32 = 4;
-
-fn print_error(msg: String) {
-    println!("{} {}", "[!]".red().bold(), msg)
-}
-
-fn get_file_path() -> String {
-    match var("TODO_FILE") {
-        Ok(val) => val,
-        Err(_) => match var("HOME") {
-            Ok(home) => format!("{}/todo.json", home),
-            Err(_) => panic!("Error reading environment variable HOME"),
-        },
-    }
-}
-
-fn get_task_id(matches: &ArgMatches) -> usize {
-    let task_id: &str = matches.value_of("id").unwrap();
-    match task_id.parse() {
-        Ok(id) => id,
-        Err(_) => {
-            print_error(format!("Invalid task id: {}", task_id));
-            exit(ERR_INVALID_TASK_ID);
-        },
-    }
-}
-
-fn get_priority(matches: &ArgMatches) -> u8 {
-    if matches.occurrences_of("priority") == 1 {
-        return match matches.value_of("priority").unwrap().parse() {
-            Ok(p) => p,
-            Err(_) => PRIORITY_DEFAULT,
-        }.clamp(PRIORITY_MIN, PRIORITY_MAX);
-    }
-
-    return PRIORITY_DEFAULT;
-}
-
-fn save_and_exit<P: AsRef<Path>>(path: P, task_list: &TaskList) {
-    match task_list.save_to_file(&path) {
-        Ok(_) => println!("Saved to {:#?}", path.as_ref().display()),
-        Err(e) => {
-            print_error(format!("{:#?}", e));
-            exit(ERR_SAVE);
-        },
-    }
-    exit(ERR_SUCCESS);
-}
-
-fn err_no_task(id: usize) {
-    print_error(format!("Could not find task with ID {}", id));
-    exit(ERR_INVALID_TASK_ID);
-}
+use crate::utils::{
+    ERR_COMPLETE, ERR_START,
+    NumberOfDigits,
+    print_error,
+    err_no_task,
+    get_priority, get_file_path, get_task_id,
+    save_and_exit,
+};
 
 fn base_command<'a>(name: &'a str, about: &'a str) -> Command<'a> {
     Command::new(name)
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,16 @@
-pub fn remove_first(text: &str) -> &str {
-    &text[1..text.len()]
-}
+use colored::Colorize;
+use std::env::var;
+use std::path::Path;
+use std::process::exit;
+use clap::ArgMatches;
+use crate::task::{PRIORITY_MAX, PRIORITY_MIN, PRIORITY_DEFAULT};
+use crate::tasklist::TaskList;
+
+pub const ERR_SUCCESS: i32 = 0;
+pub const ERR_SAVE: i32 = 1;
+pub const ERR_INVALID_TASK_ID: i32 = 2;
+pub const ERR_COMPLETE: i32 = 3;
+pub const ERR_START: i32 = 4;
 
 pub trait NumberOfDigits {
     fn number_of_digits(&self) -> usize;
@@ -20,4 +30,60 @@ impl NumberOfDigits for usize {
         else if self < &10000000000 { 10 }
         else { self.to_string().len() }
     }
+}
+
+pub fn remove_first(text: &str) -> &str {
+    &text[1..text.len()]
+}
+
+pub fn print_error(msg: String) {
+    println!("{} {}", "[!]".red().bold(), msg)
+}
+
+pub fn err_no_task(id: usize) {
+    print_error(format!("Could not find task with ID {}", id));
+    exit(ERR_INVALID_TASK_ID);
+}
+
+pub fn get_file_path() -> String {
+    match var("TODO_FILE") {
+        Ok(val) => val,
+        Err(_) => match var("HOME") {
+            Ok(home) => format!("{}/todo.json", home),
+            Err(_) => panic!("Error reading environment variable HOME"),
+        },
+    }
+}
+
+pub fn get_task_id(matches: &ArgMatches) -> usize {
+    let task_id: &str = matches.value_of("id").unwrap();
+    match task_id.parse() {
+        Ok(id) => id,
+        Err(_) => {
+            print_error(format!("Invalid task id: {}", task_id));
+            exit(ERR_INVALID_TASK_ID);
+        },
+    }
+}
+
+pub fn get_priority(matches: &ArgMatches) -> u8 {
+    if matches.occurrences_of("priority") == 1 {
+        return match matches.value_of("priority").unwrap().parse() {
+            Ok(p) => p,
+            Err(_) => PRIORITY_DEFAULT,
+        }.clamp(PRIORITY_MIN, PRIORITY_MAX);
+    }
+
+    return PRIORITY_DEFAULT;
+}
+
+pub fn save_and_exit<P: AsRef<Path>>(path: P, task_list: &TaskList) {
+    match task_list.save_to_file(&path) {
+        Ok(_) => println!("Saved to {:#?}", path.as_ref().display()),
+        Err(e) => {
+            print_error(format!("{:#?}", e));
+            exit(ERR_SAVE);
+        },
+    }
+    exit(ERR_SUCCESS);
 }
